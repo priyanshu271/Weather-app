6cb252a025a4435fb3ed84c63991a374b5a5eaa2
Priyanshu Rai <priyanshurai271@gmail.com>
2023-12-07 14:12:39 +0530

    fix: removed token

--- src/app/services/weather-data/weather-data.service.ts
+++ src/app/services/weather-data/weather-data.service.ts
@@ -11,11 +11,11 @@ export class WeatherDataService {
 
   public async getWeather(cityName: string): Promise<WeatherInfo> {
     try {
-      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=bf7521dfb4b1e05765815bf0b82e5f0f`;
+      const geoUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&appid=add your token`;
       const weatherData = await this.http.get<GeoLocation[]>(geoUrl).toPromise();
 
       if (weatherData && weatherData.length > 0) {
-        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${weatherData[0]?.lat}&lon=${weatherData[0]?.lon}&appid=bf7521dfb4b1e05765815bf0b82e5f0f`;
+        const weatherUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${weatherData[0]?.lat}&lon=${weatherData[0]?.lon}&appid=add your token`;
         const weatherInfo = await this.http.get<WeatherInfo>(weatherUrl).toPromise();
 
         if (weatherInfo) {
